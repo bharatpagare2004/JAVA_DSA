cb4f361d18c8436d2f29475129d80d0ce2d03f7c
bharatpagare2004 <bharatpagare018@gmail.com>
2024-03-08 23:05:11 +0530

    Pattern

---
+++ Inverse.java
@@ -0,0 +1,18 @@
+public class Inverse {
+
+
+    public static void main(String args[]){
+
+    int n =10;
+    for(int i = n ;i>=1 ;i--)
+    {
+        for(int j = 1; j <=i;j++)
+        {
+            System.out.print("x");
+
+        }
+        System.out.println(" ");
+    }
+    
+}
+}
