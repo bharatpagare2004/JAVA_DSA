0b95d6c2b30e2ccedef39a49224d1796eeb24727
bharatpagare2004 <bharatpagare018@gmail.com>
2024-03-09 11:45:52 +0530

    Patt_01_09

---
+++ Patt_01.java
@@ -0,0 +1,18 @@
+public class Patt_01 {
+
+
+    public static void main(String args[])
+    {
+        for(int i = 1;i<=4;i++) // outer loop 
+        {
+            for(int j =1;j<=5;j++)// inner loop
+            {
+               System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    
+}
