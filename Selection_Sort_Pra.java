679c67ca9c7f5079aaef185991dee4d30d6d2c2e
bharatpagare2004 <bharatpagare018@gmail.com>
2024-03-12 23:41:05 +0530

    sort

---
+++ Selection_Sort_Pra.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+public class Selection_Sort_Pra {
+
+    // for printing 
+    public static void print(int arr[])
+    {
+    for(int i = 0;i<arr.length;i++)
+    {
+        System.out.print(arr[i]+" ");
+    }
+    System.out.println();
+   }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the size of array:");
+        int size = sc.nextInt();
+
+       
+
+        // define the array
+        int arr[] = new int[size];
+        for(int i = 0;i<arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("enter the element:");
+        for(int i = 0;i<arr.length-1;i++)
+        {
+            int smallest = i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                  if(arr[smallest]>arr[j])
+                  {
+                    smallest = j;
+                  }
+
+                }
+                   
+                  int tem = arr[smallest];
+                  arr[smallest] = arr[i];
+                  arr[i] = tem;
+
+
+            
+        }
+        print(arr);
+       
+
+    }
+    
+}
